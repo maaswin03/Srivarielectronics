fc9c4c5904f64f0af76062f787496b68df124590
ASWIN <m.a.aswin2005@gmail.com>
2025-02-05 22:17:29 +0530

    navbar1

--- frontend/src/Components/Navigation/Navbar1.tsx
+++ frontend/src/Components/Navigation/Navbar1.tsx
@@ -1,7 +1,9 @@
+import "./Navbar1.css";
+
 export default function Navbar1() {
   return (
-    <>
-      <div>Navbar</div>
-    </>
+    <nav className="Navbar1-box">
+      <p>Mon - Sat : 9:00 AM to 7:00 PM</p>
+    </nav>
   );
 }
